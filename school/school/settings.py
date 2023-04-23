2dd4afbf7cfb65c3cc1f0958583f8349d1e00214
Arsen791 <114273777+Arsen791@users.noreply.github.com>
2023-04-23 18:09:55 +0600

    asr

--- school/school/settings.py
+++ school/school/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n+xyl3o%qcpt7ptay2=&+f0%w@5k=(g3jv_8_1ofvf=69!td1w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'arsen505.pythonanywhere.com/'
+]
 
 
 # Application definition
