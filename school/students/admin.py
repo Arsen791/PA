ae616ee696f17dfffb27b6b39e931b5b2906a639
Arsen791 <114273777+Arsen791@users.noreply.github.com>
2023-04-26 21:58:31 +0600

    add file

--- school/students/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from students.models import User_name, User_birth, User_info, User_work
-
-# Register your models here.
-admin.site.register(User_name)
-admin.site.register(User_birth)
-admin.site.register(User_info)
-admin.site.register(User_work)
-
