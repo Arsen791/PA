858838a97a87970c5f81c3a62aa6c2d5a13de55d
Arsen791 <114273777+Arsen791@users.noreply.github.com>
2023-04-27 15:33:44 +0600

    forms and html

--- it/info1/forms.py
+++ it/info1/forms.py
@@ -4,37 +4,37 @@ from django import forms
 
 class CreateNameForm(Form):
     firstname = forms.CharField(required=True,
-                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите свое имя'}))
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Арсен'}))
     secondname = forms.CharField(required=True,
                                widget=forms.TextInput(attrs={'class': 'form-control',
-                                                                 'placeholder': 'Введите свое фамилия'}))  
+                                                                 'placeholder': 'Енсегенов'}))  
 class CreateBirthForm(Form):
     date_of_birth = forms.DateField(required=True,
-                               widget=forms.DateInput(attrs={'class': 'form-control', 'placeholder': 'Введите дату своего рождения', 'type': 'date'}))
+                               widget=forms.DateInput(attrs={'class': 'form-control',  'type': 'date'}))
     place_of_birth = forms.CharField(required=True,
                                widget=forms.TextInput(attrs={'class': 'form-control',
-                                                                 'placeholder': 'Введите свое место рождения'}))
+                                                                 'placeholder': 'Сартогай'}))
     
 class CreateInfoForm(Form):
     specialization = forms.CharField(required=True,
-                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your first name'}))
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Бакалавр по специальности Математика '}))
     orga_of_education = forms.CharField(required=True,
-                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your first name'}))
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'КазНУ им. Аль-Фараби'}))
     
     year_of_graduation = forms.IntegerField(required=True,
                                widget=forms.NumberInput(attrs={'class': 'form-control',
-                                                                 'placeholder': 'Enter your second name', 'type':'number'}))
+                                                                 'placeholder': '2008', 'type':'number'}))
     
 class CreateWorkForm(Form):
     organization = forms.CharField(required=True,
-                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your organization '}))
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Декан '}))
     address = forms.CharField(required=True,
                                widget=forms.TextInput(attrs={'class': 'form-control',
-                                                                 'placeholder': 'Enter your place of birth'}))
+                                                                 'placeholder': 'Алматы Менеджмент Университет, ул.Розыбакиева, 227'}))
     
 class CreatePracticeForm(Form):
     experience = forms.IntegerField(required=True,
-                               widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter your experience '}))
+                               widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': '15 год '}))
     
 class CreateCriminalForm(Form):
     criminal_record = forms.CharField(required=True,
@@ -43,16 +43,16 @@ class CreateCriminalForm(Form):
 class CreateMedicineForm(Form):
     medicine_number = forms.CharField(required=True,
                                widget=forms.TextInput(attrs={'class': 'form-control',
-                                                                 'placeholder': 'Enter your medicine number  '}))
+                                                                 'placeholder': 'АА 12345678  '}))
 
     
 class CreateMasterForm(Form):
     master_degree = forms.CharField(required=True,
-                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Yes or No   '}))
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': ' КазНу им Аль-Фараби, магистр математики 2010 или Нет '}))
     
 class CreateDoctorForm(Form):
     doctor_profile = forms.CharField(required=True,
-                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Yes or No   '}))
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'PhD, по специальности "Информационные системы", 2015 г. или Нет  '}))
     
 class CreatePhdForm(Form):
     phd_record = forms.CharField(required=True,
@@ -60,10 +60,10 @@ class CreatePhdForm(Form):
 
 class CreateProfessorForm(Form):
     docent_professor = forms.CharField(required=True,
-                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Yes or No   '})) 
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'КазНИТУ им. К.И.Сатпаева, 6D070400 – Вычислительная техника и программное обеспечение, 2019г или Нет  '})) 
 class CreateSportForm(Form):
     sport_degree = forms.CharField(required=True,
-                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Yes or No   '}))
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '«Заслуженный тренер», 2015 или Нет  '}))
 class CreateSubjectForm(Form):
     subject_of_teaching = forms.CharField(required=True,
-                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your subject of teaching '}))
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'ИТ-стартап 1 (ideation/validation), ИТ-стартап 2 (запуск проекта, MVP, Web проект), Лаборатория Start-up '}))
