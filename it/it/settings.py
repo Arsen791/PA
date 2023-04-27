ad740195166bfad1d8f4017af0fab6b73935ec36
Arsen791 <114273777+Arsen791@users.noreply.github.com>
2023-04-27 13:19:41 +0600

    fdvs

--- it/it/settings.py
+++ it/it/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "corsheaders",
-    'rest_framework',
     'user',
     'info1'
 ]
@@ -134,7 +133,6 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
